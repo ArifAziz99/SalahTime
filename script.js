af9fe6e9aefb81465ee9db2d6a9364ef850cdf71
Mohammad Arif Aziz <md.arifaziz24599@gmail.com>
2025-04-19 21:08:06 +0530

    new function added
    added hijri - day date month ,
    gorg- day, month

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Get today's date and format it as needed
 const today = new Date();
+const city = "city=patna";
+const country = "country=india";
 const formatted = today.toLocaleDateString("en-CA"); // YYYY-MM-DD
 
 // Call the fetch function
@@ -7,7 +9,7 @@ fetchData(formatted);
 
 async function fetchData(today) {
   try {
-    const apiUrl = `https://api.aladhan.com/v1/timingsByCity/${today}?city=patna&country=india&method=8`;
+    const apiUrl = `https://api.aladhan.com/v1/timingsByCity/${today}?${city}&${country}&method=8`;
     const response = await fetch(apiUrl);
 
     if (!response.ok) throw new Error("Could not fetch resource");
@@ -22,45 +24,33 @@ async function fetchData(today) {
 function displayTime(data) {
   const {
     date: {
+      hijri: { date: hijriDate },
+      hijri: {weekday: { en: hijriDay } },
+      hijri: {month: { en: hijriMonth } },
       gregorian: { date: today },
+      gregorian: { weekday: { en: gregorianDay } },
+      gregorian: { month: { en: gregorianMonth } },
     },
     timings,
   } = data.data;
 
-  const weekday = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  const d = new Date();
-  let day = weekday[d.getDay()];
-  document.getElementById("day").innerHTML = day;
 
   const stime = document.getElementById("stime"); //stime =salah time
   stime.innerHTML = ""; // Clear previous content
 
   // Add date display
   const dateElem = document.createElement("div");
-  dateElem.className =
-    "text-center text-4xl text-white font-bold mb-10 mt-1";
-  dateElem.textContent = `${today}`;
+  dateElem.className = "text-center text-3xl text-white font-bold mb-10 mt-1";
+  dateElem.textContent = `${hijriDay} | ${hijriMonth} | ${hijriDate} ${gregorianDay} | ${gregorianMonth} | ${today} `;
   stime.appendChild(dateElem);
 
   // Loop through ALL timings
   for (const [key, value] of Object.entries(timings)) {
     const card = document.createElement("div");
-    card.className =
-      "flex items-center justify-between mb-4 bg-green-500 rounded-2xl p-6 text-center shadow-md";
+    card.className = "flex items-center justify-between mb-4 bg-green-500 rounded-2xl p-6 text-center shadow-md";
 
-    card.innerHTML = `
-        <div class="text-xl text-white font-bold">${key}</div>
-        <div class="text-4xl text-white font-bold mt-2">${value}</div>
-      `;
+    card.innerHTML = ` <div class="text-xl text-white font-bold">${key}</div>
+        <div class="text-4xl text-white font-bold mt-2">${value}</div>`;
 
     stime.appendChild(card);
   }
