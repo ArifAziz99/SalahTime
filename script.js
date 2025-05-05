f7b66207664f1469d902309676f6d2cdec2c127a
Mohammad Arif Aziz <md.arifaziz24599@gmail.com>
2025-05-05 16:14:21 +0530

    UI fix

--- script.js
+++ script.js
@@ -40,14 +40,14 @@ function displayTime(data) {
 
   // Add hijri date display
   const dateElemAR = document.createElement("div");
-  dateElemAR.className = "text-center text-xl text-black font-bold p-2 mt-5 mb-2 bg-white/30 backdrop-blur-md border border-white/20 rounded-2xl cursor-pointer";
+  dateElemAR.className = "text-center text-xl text-black font-bold px-5 p-2 mt-5 mb-2 bg-white/30 backdrop-blur-md border border-white/20 rounded-2xl cursor-pointer";
   dateElemAR.textContent = `${hijriDay} - ${hijriMonth} - ${hijriDate}`;
   stime.appendChild(dateElemAR);
 
 
     // Add gorg date display
   const dateElemEN = document.createElement("div");
-  dateElemEN.className = "text-center text-xl text-black font-semibold p-2 mb-10  bg-white/30 backdrop-blur-md border border-white/20 rounded-2xl cursor-pointer";
+  dateElemEN.className = "text-center text-xl text-black font-semibold px-5 p-2 mb-10  bg-white/30 backdrop-blur-md border border-white/20 rounded-2xl cursor-pointer";
   dateElemEN.textContent = `${gregorianDay} - ${gregorianMonth} - ${today}`;
   stime.appendChild(dateElemEN);
 
@@ -55,10 +55,10 @@ function displayTime(data) {
   // Loop through ALL timings
   for (const [key, value] of Object.entries(timings)) {
     const card = document.createElement("div");
-    card.className = "flex items-center justify-between mb-3 bg-white/30 backdrop-blur-md border border-white/20 rounded-2xl p-5 text-center transition duration-300 hover:shadow-xl transform hover:scale-105 cursor-pointer scroll-smooth";
+    card.className = "flex items-center justify-between mb-2 bg-white/30 backdrop-blur-md border border-white/20 rounded-2xl px-5 p-1 md:p-5 text-center transition duration-300 hover:shadow-xl transform hover:scale-105 cursor-pointer scroll-smooth";
 
-    card.innerHTML = ` <div class="text-2xl text-black font-semibold">${key}</div>
-        <div class="text-3xl text-black font-bold mt-2">${value}</div>`;
+    card.innerHTML = ` <div class="text-sm md:text-2xl text-black font-semibold">${key}</div>
+        <div class="text-sm md:text-2xl text-black font-bold mt-2">${value}</div>`;
 
     stime.appendChild(card);
   }
